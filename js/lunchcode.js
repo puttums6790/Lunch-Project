262794bd836a1a7134231ab487ba45327a54f82c
Jack Kennedy <puttums6790@gmail.com>
2018-01-10 20:00:19 -0600

    starting with css

---
+++ js/lunchcode.js
@@ -0,0 +1,13 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBvEEsX_emQu9Um3wzAgNZv0m2I9WBDuVg",
+    authDomain: "lunch-group-project.firebaseapp.com",
+    databaseURL: "https://lunch-group-project.firebaseio.com",
+    projectId: "lunch-group-project",
+    storageBucket: "",
+    messagingSenderId: "442112808193"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database
