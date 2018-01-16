63fa4fb3f77c5754ddea789eb171abba5e4e4375
Brendan Scruggs <brendan.scruggs@gmail.com>
2018-01-15 21:17:55 -0500

    completed working places API integration on myGroups page, although it is not needed there

---
+++ js/myGroups.js
@@ -0,0 +1,51 @@
+ // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+      var map;
+      var infowindow;
+
+      function initMap() {
+        var pyrmont = {lat: 41.896294, lng: -87.618799};
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pyrmont,
+          zoom: 15
+        });
+
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pyrmont,
+          radius: 800,
+          type: ['restaurant']
+        }, callback);
+      }
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+            console.log(results[i]);
+            $("#well").append(
+            	"<h2>" + results[i].name + "</h2>" +
+            	"<p><strong>Rating: </strong>" + results[i].rating + "</p>" +
+            	"<p><strong>Price Level: </strong>" + results[i].price_level + "</p>" +
+            	"<p><strong>Address: </strong>" + results[i].vicinity + "</p>" +
+            	"<br>");
+          }
+        }
+      }
+
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+      }
