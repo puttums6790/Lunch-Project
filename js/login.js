690c48c82283207e331b12a4ef3244509fac47b4
Madeleine Griffin <madeleinecgriffin@gmail.com>
2018-01-17 19:26:48 -0600

    consolidating login stuff for firebase

--- js/login.js
+++ js/login.js
@@ -1,62 +1,79 @@
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBvEEsX_emQu9Um3wzAgNZv0m2I9WBDuVg",
-    authDomain: "lunch-group-project.firebaseapp.com",
-    databaseURL: "https://lunch-group-project.firebaseio.com",
-    projectId: "lunch-group-project",
-    storageBucket: "",
-    messagingSenderId: "442112808193"
-  };
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBvEEsX_emQu9Um3wzAgNZv0m2I9WBDuVg",
+  authDomain: "lunch-group-project.firebaseapp.com",
+  databaseURL: "https://lunch-group-project.firebaseio.com",
+  projectId: "lunch-group-project",
+  storageBucket: "",
+  messagingSenderId: "442112808193"
+};
 
-  firebase.initializeApp(config);
-  var database = firebase.database();
+firebase.initializeApp(config);
+var database = firebase.database();
 
-  //sets users initial logged in status based on session storage
-  var isLoggedIn = false;
-  
-  function checkLogin(path) {
-    if(path) {
+//sets users initial logged in status based on session storage
+var isLoggedIn = false;
+
+/*if (sessionStorage.getItem(isLoggedIn) == null) {
+  isLoggedIn = false;
+}
+isLoggedIn = sessionStorage.getItem("isLoggedIn");
+
+if (isLoggedIn == "false") {
+  isLoggedIn = false;
+}
+
+if (isLoggedIn == "true") {
+  isLoggedIn = true;
+}*/
+
+//initializing variables
+var buttonPressed;
+var newLink;
+var userExists;
+var place;
+var date;
+var today;
+
+
+//function to check if user is logged in already
+function checkLogin(path) {
+  if(path) {
     if (isLoggedIn) {
       window.location = path +'.html';
     } else {
       //window.location.href("Lunch.html")
       $('#loginScreen').show();
     }
-    } else {
-      if(isLoggedIn) {
-        window.location = "Lunch.html";
-      }
+  } else {
+    if(isLoggedIn) {
+      window.location = "Lunch.html";
     }
   }
+}
 
-  /*if (sessionStorage.getItem(isLoggedIn) == null) {
-    isLoggedIn = false;
-  }
-  isLoggedIn = sessionStorage.getItem("isLoggedIn");
+function addGroup() {
 
-  if (isLoggedIn == "false") {
-    isLoggedIn = false;
-  }
+  place = $("#myGroup").text();
 
-  if (isLoggedIn == "true") {
-    isLoggedIn = true;
-  }*/
+  today = new Date();
+  date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
-  var buttonPressed;
-  var newLink;
-  var userID = 0;
-  var userExists;
-
-  $('#top-login-overlay, #top-login-modal span').click(function(){
-    $('#top-login-overlay, #top-login-modal').hide();
+  database.ref().child("Users").child(username).child("MyGroups").set({
+    place: place,
+    time: time
   });
+
+}
 
 //when a home screen button is clicked
 $(".btn-lg").on("click", function(event) {
 
   //determins id of button pressed
   buttonPressed = $(this).attr("id");
+
+  //checks if logged in
   checkLogin(buttonPressed);
 
 })
@@ -78,7 +95,6 @@ $("#submitLogin").on("click", function(event) {
 
   //sets login status to true
   isLoggedIn = true;
-  userID++;
   sessionStorage.setItem("isLoggedIn", isLoggedIn);
 
   database.ref().orderByChild("username").equalTo(username).once("value",snapshot => {
@@ -92,14 +108,16 @@ $("#submitLogin").on("click", function(event) {
     //if user does not already exist in firebase, save data to firebase
     else {
       console.log("does not exist");
-      database.ref().push({
-        userID: "user"+userID,
+      database.ref().child("Users").child(username).set({
         username: username,
         password: password,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
       });
     }
+
+    //check if user is logged in
     checkLogin();
+
   });
 
 })
