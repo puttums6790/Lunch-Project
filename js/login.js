d5acbee97bdffe6372adef6c96259ba1a3f83e88
Madeleine Griffin <madeleinecgriffin@gmail.com>
2018-01-20 12:09:04 -0600

    lots of firebase updates - updating my group page

--- js/login.js
+++ js/login.js
@@ -12,10 +12,22 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-//sets users initial logged in status based on session storage
-var isLoggedIn = false;
 
-/*if (sessionStorage.getItem(isLoggedIn) == null) {
+
+//initializing variables
+var buttonPressed;
+var newLink;
+var userExists;
+var groupName;
+var groupTheme;
+var groupParticipants;
+var groupDate;
+var isLoggedIn;
+
+
+
+//sets users initial logged in status based on session storage
+if (sessionStorage.getItem(isLoggedIn) == null) {
   isLoggedIn = false;
 }
 isLoggedIn = sessionStorage.getItem("isLoggedIn");
@@ -26,78 +38,118 @@ if (isLoggedIn == "false") {
 
 if (isLoggedIn == "true") {
   isLoggedIn = true;
-}*/
+}
 
-//initializing variables
-var buttonPressed;
-var newLink;
-var userExists;
-var groupName;
-var groupTheme;
-var groupParticipants;
-var groupDate;
 
 
 //function to check if user is logged in already
 function checkLogin(path) {
+  
+  //if a button to go to a specifc page is clicked
   if(path) {
+
+    //goes to link location page if user is logged in
     if (isLoggedIn) {
       window.location = path +'.html';
-    } else {
-      //window.location.href("Lunch.html")
+      
+    } 
+    //shows login box if user is not logged in
+    else {
       $('#loginScreen').show();
     }
-  } else {
+  } 
+
+  //goes back to home page after submit button clicked on log in screen
+  else {
     if(isLoggedIn) {
       window.location = "Lunch.html";
     }
   }
 }
 
-function addGroup() {
-  
-  groupName = $("#groupName").val().trim();
-  groupParticipants = $("#groupParticipants").val().trim();
-  groupTheme = $("#groupTheme").val().trim();
-  console.log(groupParticipants);
-  console.log(groupTheme);
+
+
+//makes table of your groups - runs when My Groups page loads
+function refreshMyGroups() {
+
+  //get username from session storage
   var username = sessionStorage.getItem("username", username);
 
-  today = new Date();
-  groupDate = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  //pull user's groups from firebase
+  database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    const userData = childSnapshot.val()[username]["MyGroups"];
+    var tableCount = 0;
 
+    //loops through firebase groups to display in table
+    for(var groupId in userData) {
+      tableCount++
+      var id = groupId;
+      var data = userData[id]
+
+      //appends each firebase stored element to it's respective table column
+      $("#myGroupsTable > tbody").append("<tr><td>" + tableCount + "</td><td>" 
+        + userData[id].groupName + "</td><td>" + userData[id].groupDate + "</td><td>" 
+        + "hold for time" + "</td><td>" + userData[id].groupParticipants + "</td><td>" 
+        + userData[id].groupTheme + "</td><td>" +"hold for restaurant"+ "</td><td>" 
+        + "hold for address" + "</td><td>" +"<button class='leaveButton'id="+ id +">Remove Group</button>"+"</td></tr>");
+    }
+
+    //if a delete group button is clicked, removes it from firebase and reloads page
+    $(".leaveButton").on("click", function(event) {
+      event.preventDefault();
+      var removeID = $(this).attr("id");
+      database.ref().child("Users").child(username).child("MyGroups").child(removeID).remove();
+      window.location = 'myGroups.html';
+    })
+  })
+}
+
+
+
+//function add a new group - runs when "create group" button is clicked
+function addGroup() {
+
+  //gets values for group info from user input
+  var groupName = $("#groupName").val().trim();
+  groupParticipants = $("#groupParticipants").val().trim();
+  groupTheme = $("#groupTheme").val().trim();
+  groupDate = $("#groupDate").val().trim();
+
+  //gets username from session storage
+  var username = sessionStorage.getItem("username", username);
+
+  //adds group info to firebase
   database.ref().child("Users").child(username).child("MyGroups").push({
     groupName: groupName,
     groupDate: groupDate,
     groupParticipants: groupParticipants,
     groupTheme: groupTheme
   });
-  
-  database.ref().orderByChild("username").equalTo(username).once("value",snapshot => {
-    const userData = snapshot.val().child("Users").child(username).child("MyGroups");
-    console.log(userData)
-  })
-
-  console.log(userData);
-
 }
 
-//when a home screen button is clicked
+
+
+//on home screen, if button on nav bar or to go to another page is clicked
 $(".btn-lg").on("click", function(event) {
 
   //determins id of button pressed
   buttonPressed = $(this).attr("id");
 
-  //checks if logged in
+  //checks if logged in and acts according to function
   checkLogin(buttonPressed);
 
 })
 
+
+
+//when the Create Group button is clicked run add Group function and return to create group screen
 $("#createGroupBtn").on("click", function(event) {
   event.preventDefault();
   addGroup();
   window.location = 'CreateGroup.html';
 })
+
+
 
 //submitting username and login to log in
 $("#submitLogin").on("click", function(event) {
@@ -114,10 +166,11 @@ $("#submitLogin").on("click", function(event) {
   sessionStorage.setItem("username", username);
   sessionStorage.setItem("password", password);
 
-  //sets login status to true
+  //sets login status to true (not secure for real life but it works!)
   isLoggedIn = true;
   sessionStorage.setItem("isLoggedIn", isLoggedIn);
 
+  //looks through all usernames stored in firebase
   database.ref().orderByChild("username").equalTo(username).once("value",snapshot => {
     const userData = snapshot.val();
     
@@ -128,7 +181,6 @@ $("#submitLogin").on("click", function(event) {
 
     //if user does not already exist in firebase, save data to firebase
     else {
-      console.log("does not exist");
       database.ref().child("Users").child(username).set({
         username: username,
         password: password,
@@ -142,4 +194,8 @@ $("#submitLogin").on("click", function(event) {
 
 })
 
+
+
+//refreshes my groups list on load of page - this will display on My Groups table
+refreshMyGroups()
 
